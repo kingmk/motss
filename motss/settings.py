2a141a5417188b443f63ab056a4cc0066c824049
kingmk <mk_king813@163.com>
2013-04-14 14:25:02 +0800

    untrack some files

--- motss/settings.py
+++ motss/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'member',
+    'post',
 )
 
 # A sample logging configuration. The only tangible logging
