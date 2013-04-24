cdd866c0732c7456c013f622a60108e66ddce105
kingmk <mk_king813@163.com>
2013-04-24 11:47:58 +0800

    change settings to support static files, templates dir

--- motss/settings.py
+++ motss/settings.py
@@ -1,4 +1,7 @@
 # Django settings for motss project.
+import os
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -112,6 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates')
 )
 
 INSTALLED_APPS = (
--- motss/urls.py
+++ motss/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from motss import views
+from motss import settings
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',    
@@ -8,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^member/', include('member.urls', namespace="member")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^message/', include('short_message.urls', namespace="short_message")),
+    url(r'^test/',views.test,name='test'),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
